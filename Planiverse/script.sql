7a4f368c6dee7513e6e0d41cbb2080b1d8bf6156
Heey2 <155238712+Heey2@users.noreply.github.com>
2024-04-19 22:47:12 +0900

    query script 정리

--- Planiverse/script.sql
+++ Planiverse/script.sql
@@ -1,19 +1,35 @@
-
 alter session set "_oracle_script" = true;
 grant connect, resource, dba to planiverse;
 create user planiverse identified by java1234;
 
+--여기부터 시작
 create table tblUser(
     id VARCHAR2(100) primary key,
     pw VARCHAR2(50) not null,
     name VARCHAR2(20) not null
 );
+--아래는 CRUD
+insert into tblUser values ('test', '1111', '테스트');
+
+select * from tblUser;
+
+update tblUser set pw = ? where id = ?;
+
+delete from tblUser where id = ?;
 --------------------------------------------------------------유저테이블
 create table tblCalList(
     calListSeq number primary key,
     id VARCHAR2(100) not null references tblUser(id)
 );
 create sequence seqCalList;
+--아래는 CRUD
+insert into tblCalList values (1, 'test');
+
+select * from tblCalList;
+
+update tblCalList set id = ? where calListSeq = ?;
+
+delete from tblCalList where calListSeq = ?;
 ---------------------------------------------------------------달력리스트
 create table tblCal (
     calSeq number primary key,
@@ -23,11 +39,29 @@ create table tblCal (
     check (shareInfo in ('y', 'n'))
 );
 create sequence seqCal;
+--아래는 CRUD
+insert into tblCal values (1, 'n', 'test', 1);
+
+select * from tblCal;
+
+update tblCal set shareInfo = ? where calSeq = ?;
+
+delete from tblCal where calSeq = ?;
 ----------------------------------------------------------------달력
 create table tblColor(
     color VARCHAR2(10) primary key,
     name VARCHAR2(30)
 );
+--아래는 CRUD
+insert into tblColor values ('#F1932E', '주황');
+insert into tblColor values ('#A8D8EA', '하늘');
+insert into tblColor values ('#AA96DA', '보라');
+insert into tblColor values ('#F9B8D1', '분홍');
+insert into tblColor values ('#FFFFD2', '노랑');
+
+select * from tblColor;
+
+delete from tblCal where color = ?;
 ----------------------------------------------------------------컬러
 create table tblEvent (
     eventSeq number primary key,
@@ -42,49 +76,38 @@ create table tblEvent (
     check (allDay in ('y', 'n'))
 );
 create sequence seqEvent;
+--아래는 CRUD
 insert into tblEvent(eventSeq, title, allDay, "start", "end", loc, "content", color, calSeq) 
 values (seqEvent.nextVal, 'Party', 'n', '2023-01-05 20:00:00','', '우리집','재밌겠다','#A8D8EA',1);
+
+select * from tblEvent;
+
+update tblEvent set title = ? where eventSeq = ?;
+
+delete from tblEvent where eventSeq = ?;
 ----------------------------------------------------------------일정
 create table tblShare (
     id VARCHAR2(100) not null references tblUser(id),
     calSeq NUMBER references tblCal(calSeq) not null,
     shareTk VARCHAR2(1000)
 );
+--아래는 CRUD
+insert into tblShare values ('test', ?, '');
+
+select * from tblShare;
+
+update tblShare set shareTk =? where id =? and calSeq = ?;
+
+delete from tblShare where id =? and calSeq = ?;
 ----------------------------------------------------------------공유여부
-select * from tblEvent;
-
-insert into tblUser values ('test', '1111', '테스트');
-
---
-
-insert into tblCalList values (1, 'test');
---
-
-
-insert into tblCal values (1, 'n', 'test', 1);
-
---
-
-insert into tblColor values ('#F1932E', '주황');
-insert into tblColor values ('#A8D8EA', '하늘');
-insert into tblColor values ('#AA96DA', '보라');
-insert into tblColor values ('#F9B8D1', '분홍');
-insert into tblColor values ('#FFFFD2', '노랑');
-
-delete from tblColor where name='주황';
---
-update tblEvent set colSeq = '#AA96DA' where eventSeq=1;
-
-
 
 commit;
-select * from tblEvent;
-update tblEvent set "start"='2023-01-23 20:00',"end"='' where eventSeq = 1;
+
 rollback;
 
 
 -----------------------------여기까지 정보 넣으면 테스트 가능
-
+--아래는 논의 필요
 
 
 create table tblWhat (
